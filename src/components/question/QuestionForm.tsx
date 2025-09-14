44adb77a9dc8b8efef8ee05e6fba09e475eb7418
matsumoto <sora17453317@icloud.com>
2025-09-14 21:56:01 +0900

    [mod] 初期画面をcalendarに変更, 初期画面変更に伴う不要ファイルの作成

--- src/components/question/QuestionForm.tsx
+++ src/components/question/QuestionForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-// import styles from "@/app/page.module.css";
 
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
